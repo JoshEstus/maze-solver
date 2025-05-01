a115249e5434cf8f551ff6aa27e04ef06c408dbb
JoshEstus <josh.estus@gmail.com>
2025-05-01 16:44:11 -0400

    Add base test, and add maze drawing with cells

--- src/drawing.py
+++ src/drawing.py
@@ -16,7 +16,7 @@ class Line():
 
 class Cell():
 
-    def __init__(self, win):
+    def __init__(self, win=None):
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
@@ -28,6 +28,12 @@ class Cell():
         self.window = win
 
     def draw(self, x1, x2, y1, y2):
+        if self.window is None:
+            return
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
         if self.has_left_wall:
             line = Line(Point(x1, y1), Point(x1, y2))
             self.window.draw_line(line)
@@ -40,3 +46,18 @@ class Cell():
         if self.has_top_wall:
             line = Line(Point(x1, y1), Point(x2, y1))
             self.window.draw_line(line)
+
+
+    def draw_move(self, to_cell, undo=False):
+        color = "red"
+        if undo:
+            color = "gray"
+        half_length = abs(self._x2 - self._x1) // 2
+        x_center = half_length + self._x1
+        y_center = half_length + self._y1
+
+        half_length2 = abs(to_cell._x2 - to_cell._x1) // 2
+        x_center2 = half_length2 + to_cell._x1
+        y_center2 = half_length2 + to_cell._y1
+        line = Line(Point(x_center, y_center), Point(x_center2, y_center2))
+        self.window.draw_line(line, color)
