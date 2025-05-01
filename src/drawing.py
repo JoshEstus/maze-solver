f515af0abe4062341bf1cbf91e3f6b106a5bbf24
JoshEstus <josh.estus@gmail.com>
2025-05-01 16:08:44 -0400

    Draw a blue line and init window and points/lines

--- src/drawing.py
+++ src/drawing.py
@@ -12,3 +12,31 @@ class Line():
 
     def draw(self, canvas, fill_color):
         canvas.create_line(self.point1.x, self.point1.y, self.point2.x, self.point2.y, fill=fill_color, width=2)
+
+
+class Cell():
+
+    def __init__(self, win):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
+        self.window = win
+
+    def draw(self, x1, x2, y1, y2):
+        if self.has_left_wall:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self.window.draw_line(line)
+        if self.has_right_wall:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self.window.draw_line(line)
+        if self.has_bottom_wall:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self.window.draw_line(line)
+        if self.has_top_wall:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self.window.draw_line(line)
--- src/main.py
+++ src/main.py
@@ -1,13 +1,16 @@
 from window import Window
-from drawing import Point, Line
+from drawing import Point, Line, Cell
 
 
 def main():
     win = Window(800, 600)
-    point1 = Point(1, 2)
+    point1 = Point(350, 450)
     point2 = Point(400, 500)
-    line1 = Line(point1, point2)
-    win.draw_line(line1, "blue")
+    cell = Cell(win)
+    cell.has_bottom_wall = True
+    cell.has_top_wall = False
+    cell.has_left_wall = False
+    cell.draw(point1.x, point2.x, point1.y, point2.y)
     win.wait_for_close()
 
 
