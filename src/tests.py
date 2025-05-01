1f8942d19b31f06874e37cdc160d65916b230808
JoshEstus <josh.estus@gmail.com>
2025-05-01 16:45:34 -0400

    Add more tests for constructor

--- src/tests.py
+++ src/tests.py
@@ -15,5 +15,31 @@ class Tests(unittest.TestCase):
             num_rows,
         )
 
+    def test_maze_create_cells_with_more_cols(self):
+        num_cols = 200
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            len(m1.cells),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1.cells[0]),
+            num_rows,
+        )
+
+    def test_maze_create_cells_with_more_col_length(self):
+        num_cols = 200
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 100)
+        self.assertEqual(
+            len(m1.cells),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1.cells[0]),
+            num_rows,
+        )
+
 if __name__ == "__main__":
     unittest.main()
